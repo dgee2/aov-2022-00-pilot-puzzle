671e4a80db45ed3277bbe0a470633540f70d866c
dgee2 <dgee2@users.noreply.github.com>
2022-11-23 22:38:39 +0000

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="w-full h-full flex justify-center items-center">
+    <div class="w-full h-full grid grid-cols-3 place-items-stretch">
+      <div class="flex items-center justify-center border-r-2 border-b-2">1</div>
+      <div class="flex items-center justify-center border-x-2 border-b-2">2</div>
+      <div class="flex items-center justify-center border-l-2 border-b-2">3</div>
+      <div class="flex items-center justify-center border-r-2 border-y-2">4</div>
+      <div class="flex items-center justify-center border-2">5</div>
+      <div class="flex items-center justify-center border-l-2 border-y-2">6</div>
+      <div class="flex items-center justify-center border-r-2 border-t-2">7</div>
+      <div class="flex items-center justify-center border-x-2 border-t-2">8</div>
+      <div class="flex items-center justify-center border-l-2 border-t-2">9</div>
+    </div>
+  </div>
+</template>
