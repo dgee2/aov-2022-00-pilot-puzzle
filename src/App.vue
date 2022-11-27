b1bf5a9ec799b3eb8aada6a62ad2465a6d9919f4
dgee2 <dgee2@users.noreply.github.com>
2022-11-27 12:47:39 +0000

    1 file modified

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,32 @@
 <template>
   <div class="w-full h-full flex justify-center items-center">
     <div class="grid grid-cols-3 place-items-stretch">
-      <div class="flex items-center justify-center border-r-2 border-b-2">1</div>
-      <div class="flex items-center justify-center border-x-2 border-b-2">2</div>
-      <div class="flex items-center justify-center border-l-2 border-b-2">3</div>
-      <div class="flex items-center justify-center border-r-2 border-y-2">4</div>
-      <div class="flex items-center justify-center border-2">5</div>
-      <div class="flex items-center justify-center border-l-2 border-y-2">6</div>
-      <div class="flex items-center justify-center border-r-2 border-t-2">7</div>
-      <div class="flex items-center justify-center border-x-2 border-t-2">8</div>
-      <div class="flex items-center justify-center border-l-2 border-t-2">9</div>
+      <div class="flex items-center justify-center border-r-2 border-b-2 text-9xl">{{ topLeft }}</div>
+      <div class="flex items-center justify-center border-x-2 border-b-2 text-9xl"></div>
+      <div class="flex items-center justify-center border-l-2 border-b-2 text-9xl"></div>
+      <div class="flex items-center justify-center border-r-2 border-y-2 text-9xl"></div>
+      <div class="flex items-center justify-center border-2 text-9xl">5</div>
+      <div class="flex items-center justify-center border-l-2 border-y-2 text-9xl">6</div>
+      <div class="flex items-center justify-center border-r-2 border-t-2 text-9xl">7</div>
+      <div class="flex items-center justify-center border-x-2 border-t-2 text-9xl">8</div>
+      <div class="flex items-center justify-center border-l-2 border-t-2 text-9xl">9</div>
     </div>
   </div>
 </template>
+
+<script>
+const defaultGame = {
+  topLeft: 'topLeft',//&nbsp;,
+  topMiddle: &nbsp;,
+  topRight: &nbsp;,
+  middleLeft: &nbsp;,
+  middleMiddle: &nbsp;,
+  middleRight: &nbsp;,
+  bottomLeft: &nbsp;,
+  bottomMiddle: &nbsp;,
+  bottomRight: &nbsp;,
+}
+export default {
+  data() {return JSON.parse(JSON.stringify(defaultGame)}
+}
+</script>
